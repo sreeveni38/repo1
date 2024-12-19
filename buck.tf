d8f024487dec68f5db6259c10866db4c2c059423
sreeveni38 <sreeveni0310@gmail.com>
2024-12-19 16:37:00 +0530

    buck commit

---
+++ buck.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "krish-bucket" {
+  bucket = "my-bucket-state-sri-kittu"
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.krish-bucket.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["992382790263"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.krish-bucket.arn,
+      "${aws_s3_bucket.krish-bucket.arn}/*",
+    ]
+  }
+}
